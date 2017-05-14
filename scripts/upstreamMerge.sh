03fa814e10285efd5049371af32aba19e1336b09
mastercoms <circuitsoft@tuta.io>
2017-05-14 09:58:29 -0400

    1.12

--- scripts/upstreamMerge.sh
+++ scripts/upstreamMerge.sh
@@ -8,13 +8,13 @@ workdir="$basedir/work"
 
 function update {
     cd "$workdir/$1"
-    git fetch && git reset --hard origin/master
+    git fetch && git reset --hard origin/$2
     cd ../
     git add $1
 }
 
-update Bukkit
-update Spigot
-update Paper
+update Bukkit master
+update Spigot master
+update Paper pre/1.12
 
 )
