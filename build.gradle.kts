1c7335b7de1a234cca72061272f4d2b3dd071494
mastercoms <mastercoms@tuta.io>
2022-06-17 06:10:28 -0400

    fully use paperweight

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,70 +1,115 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import io.papermc.paperweight.util.Git
+import io.papermc.paperweight.patcher.tasks.PaperweightPatcherPrepareForDownstream
+
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.core") version "1.1.8"
+    `maven-publish`
+    id("io.papermc.paperweight.patcher") version "1.3.7"
 }
 
-subprojects {
+val javaVersion = 17
+
+allprojects {
     apply(plugin = "java")
     apply(plugin = "maven-publish")
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(16))
+            languageVersion.set(JavaLanguageVersion.of(javaVersion))
         }
     }
+}
 
+subprojects {
     tasks.withType<JavaCompile> {
         options.encoding = Charsets.UTF_8.name()
-        options.release.set(16)
+        options.release.set(javaVersion)
     }
     tasks.withType<Javadoc> {
         options.encoding = Charsets.UTF_8.name()
+
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+
+        exclude("**/*.xml")
     }
     tasks.withType<ProcessResources> {
         filteringCharset = Charsets.UTF_8.name()
     }
-
-    configure<PublishingExtension> {
-        repositories {
-            maven {
-                name = "glowstoneSnapshots"
-                url = uri("https://repo.glowstone.net/content/repositories/snapshots/")
-                credentials(PasswordCredentials::class)
-            }
+    tasks.withType<Test> {
+        testLogging {
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+            events(TestLogEvent.STANDARD_OUT)
         }
     }
 
     repositories {
+        maven("https://repo.glowstone.net/repository/maven-public/")
+        maven("https://repo.glowstone.net/repository/snapshots/")
         maven("https://repo.glowstone.net/repository/internal/")
-        mavenCentral()
-        maven("https://repo1.maven.org/maven2/")
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
     }
-}
-
-repositories {
-    maven("https://repo.glowstone.net/repository/internal/")
-    mavenCentral()
 }
 
 dependencies {
 }
 
-paperweight {
-    minecraftVersion.set(providers.gradleProperty("mcVersion"))
+val paperDir = layout.projectDirectory.dir("work/Paper")
+val initSubmodules by tasks.registering {
+    outputs.upToDateWhen { false }
+    doLast {
+        Git(layout.projectDirectory)("submodule", "update", "--init").executeOut()
+    }
+}
 
-    paper {
+val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
+
+paperweight {
+    mcDevSourceDir.set(layout.buildDirectory.dir("ignore"))
+
+    remapRepo.set(paperMavenPublicUrl)
+    decompileRepo.set(paperMavenPublicUrl)
+
+    upstreams {
+        register("paper") {
+            upstreamDataTask {
+                dependsOn(initSubmodules)
+                projectDir.set(paperDir)
+            }
+            patchTasks {
+                register("api") {
+                    upstreamDir.set(paperDir.dir("Paper-API"))
+                    patchDir.set(layout.projectDirectory.dir("Paper-API-Patches"))
+                    outputDir.set(layout.projectDirectory.dir("Glowkit"))
+                    importMcDev.set(false)
+                }
+            }
+        }
+    }
+}
+
+allprojects {
+    publishing {
+        repositories {
+            maven("https://repo.glowstone.net/content/repositories/snapshots/") {
+                name = "glowstone"
+                credentials(PasswordCredentials::class)
+            }
+        }
     }
 }
 
 tasks.register("printMinecraftVersion") {
     doLast {
         println(providers.gradleProperty("mcVersion").get().trim())
+    }
+}
+
+tasks.register("printGlowkitVersion") {
+    doLast {
+        println(project.version)
     }
 }
