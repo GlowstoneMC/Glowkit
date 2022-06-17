862ad95a0d73b15958f4a56c698cc0c0177be56b
mastercoms <mastercoms@tuta.io>
2022-06-17 06:19:44 -0400

    only use paper submodule

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -7,22 +7,12 @@ function changelog() {
     base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
     cd $1 && git log --oneline ${base}...HEAD
 }
-bukkit=$(changelog work/Bukkit)
-spigot=$(changelog work/Spigot)
 paper=$(changelog work/Paper)
 
 updated=""
 logsuffix=""
-if [ ! -z "$bukkit" ]; then
-    logsuffix="$logsuffix\n\nBukkit Changes:\n$bukkit"
-    updated="Bukkit"
-fi
-if [ ! -z "$spigot" ]; then
-    logsuffix="$logsuffix\n\nSpigot Changes:\n$spigot"
-    if [ -z "$updated" ]; then updated="Spigot"; else updated="$updated/Spigot"; fi
-fi
 if [ ! -z "$paper" ]; then
-    logsuffix="$logsuffix\n\nPaper Changes:\n$spigot"
+    logsuffix="$logsuffix\n\nPaper Changes:\n$paper"
     if [ -z "$updated" ]; then updated="Paper"; else updated="$updated/Paper"; fi
 fi
 
