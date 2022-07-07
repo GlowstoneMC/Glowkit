e1b5e7cf1510a32df0cf1d734170c173d778714e
mastercoms <mastercoms@tuta.io>
2022-07-07 16:14:10 -0400

    only commit if API was updated

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -5,7 +5,7 @@ PS1="$"
 
 function changelog() {
     base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
-    cd $1 && git log --oneline ${base}...HEAD
+    cd $1 && git log --oneline ${base}..HEAD -- patches/api
 }
 paper=$(changelog work/Paper)
 
@@ -14,6 +14,10 @@ logsuffix=""
 if [ ! -z "$paper" ]; then
     logsuffix="$logsuffix\n\nPaper Changes:\n$paper"
     if [ -z "$updated" ]; then updated="Paper"; else updated="$updated/Paper"; fi
+fi
+
+if [ -z "$updated" ]; then
+  exit 0
 fi
 
 log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${logsuffix}"
